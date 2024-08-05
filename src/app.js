edad9859e9ef05e29593e610fd84ac0e788850b6
Joan Figueira <joanfigueira365@gmail.com>
2024-08-05 19:12:44 +0000

    Feat: Lineamiento de codigo

--- src/app.js
+++ src/app.js
@@ -1,35 +1,34 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let ext = [".com", ".org", ".es"];
+const pronoun = ["the", "our"];
+const adjective = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const extension = [".com", ".org", ".es"];
 
 function domain() {
-  let fulldomain = "";
+  let fullDomain = "";
   for (let index1 = 0; index1 < pronoun.length; index1++) {
-    const element1 = pronoun[index1];
+    const pronounDomain = pronoun[index1];
 
-    for (let index2 = 0; index2 < adj.length; index2++) {
-      const element2 = element1 + adj[index2];
+    for (let index2 = 0; index2 < adjective.length; index2++) {
+      const adjectiveDomain = +adjective[index2];
 
       for (let index3 = 0; index3 < noun.length; index3++) {
-        const element3 = element2 + noun[index3];
+        const nounDomain = element2 + noun[index3];
 
-        for (let index4 = 0; index4 < ext.length; index4++) {
-          const element4 = element3 + ext[index4];
+        for (let index4 = 0; index4 < extension.length; index4++) {
+          const extensionDomain = element3 + extension[index4];
 
           let mytag = `<li class="list-group-item" id="generator">${element4}</li>`;
-          fulldomain += mytag;
+          fullDomain += mytag;
         }
       }
     }
   }
-  document.querySelector("#generator").innerHTML = fulldomain;
+  document.querySelector("#generator").innerHTML = fullDomain;
 }
 domain();
